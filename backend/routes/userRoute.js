ef37170785772b34ec7e3afc64d9dd509a792a74
Coder Kaushik <2022kucp1077@iiitkota.ac.in>
2025-06-08 12:13:17 +0530

    cookie fix #6

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -47,7 +47,7 @@ router.get(
           res.cookie('token', token, {
               httpOnly: true,
               secure: process.env.NODE_ENV === 'production',
-              sameSite: 'lax',
+              sameSite: 'none',
               expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) // 7 days
           });
 
