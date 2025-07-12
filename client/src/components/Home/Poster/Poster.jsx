7b975081fd591f590693b92b7f7fdd1b63026b5e
Coder Kaushik <2022kucp1077@iiitkota.ac.in>
2025-07-13 01:01:37 +0530

    added new poster on home page (hitesh)

--- client/src/components/Home/Poster/Poster.jsx
+++ client/src/components/Home/Poster/Poster.jsx
@@ -4,7 +4,7 @@ import Image from './poster.png'
 const Poster = () => {
     return (
         <div className='w-full h-auto py-6 flex justify-center items-center'>
-            <img src={Image} alt="Poster" />
+            <a href='/product/6870e1e11022eca9beb2b1c6'><img src={Image} alt="Poster" /></a>
         </div>
     );
 };
